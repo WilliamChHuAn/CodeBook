d92005500440126b64cc264981a4739df8614314
CHA <76138565+WilliamChHuAn@users.noreply.github.com>
2021-11-03 15:13:06 +0800

    update

--- Contents/DP/Pascal.cpp
+++ Contents/DP/Pascal.cpp
@@ -1,13 +1,11 @@
 // init：f(i, 0) = f(i, i) = 1
 // tren：f(i, j) = f(i - 1, j) + f(i - 1, j - 1)
-int main(){
-    int dp[30][30];
-    memset(dp, 0, sizeof(dp));
-    for (int i = 0; i < 30; ++i){
+#define N 30
+int dp[N][N];
+void Pascal_Traingle(void){
+    for(int i = 0; i < N; i++){
         dp[i][0] = dp[i][i] = 1;
-    }
-    for (int i = 1; i < 30; ++i){
-        for (int j = 1; j < 30; ++j){
+        for(int j = 1; j < i; j++){
             dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
         }
     }
